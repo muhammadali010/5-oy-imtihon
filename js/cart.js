ebf3f452b85fb243e31ada3ea48f5dbf213209c0
muhammadali010 <muhammadjonovmuhammadali32@gmail.com>
2024-07-12 02:27:34 +0500

    tugadi

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,13 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+    const deleteButton = document.querySelector('.udalit');
+    
+    if (deleteButton) {
+        deleteButton.addEventListener('click', function() {
+            const korzina = document.getElementById('korzina');
+            if (korzina) {
+                confirm('Ochirishga rozimisiz')
+                korzina.style.display = 'none';
+            }
+        });
+    }
+});
